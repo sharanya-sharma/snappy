50618497283f8f8cf0bccce8545e4f1b6c6b6819
sharanya-sharma <137407321+sharanya-sharma@users.noreply.github.com>
2024-07-22 20:47:09 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = `${window.location.origin}`;
+export const host = "http://localhost:5000";
 
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
