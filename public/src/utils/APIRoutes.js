efd46366f1f7c40db63dcbc23a354cd89bfc336f
sharanya-sharma <137407321+sharanya-sharma@users.noreply.github.com>
2024-07-24 13:11:45 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://snappy-backend-8zeb.onrender.com";
 
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
